a4f7b5a1139e30517410c85e6342517b1f531cd9
Alecsandro Schopf Auer Junior <alexschopf05@gmail.com>
2024-09-12 14:45:37 -0300

    build: add necessary dependencies to project

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import PrimeVue from 'primevue/config';
 import router from './router'
+import Lara from '@primevue/themes/lara';
 
-const app = createApp(App)
+const app = createApp(App);
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Lara,
+        options: {
+            prefix: 'p',
+            cssLayer: false,
+            darkModeSelector: '.dark'
+        },
+    }
+});
 
 app.use(router)
 
-app.mount('#app')
+app.mount('#app');
